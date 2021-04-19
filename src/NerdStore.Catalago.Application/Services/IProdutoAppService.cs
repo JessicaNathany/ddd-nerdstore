c566a0fcf84b1acc67aeca01e479453934c7a5e4
Jéssica <jessicanathany.web@gmail.com>
2021-04-19 07:58:05 -0300

    criação do projeto application, criação dos mapeamentos e services

---
+++ src/NerdStore.Catalago.Application/Services/IProdutoAppService.cs
@@ -0,0 +1,19 @@
+﻿using NerdStore.Catalago.Application.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NerdStore.Catalago.Application.Services
+{
+    public interface IProdutoAppService : IDisposable
+    {
+        Task<IEnumerable<ProdutoViewModel>> ObterPorCategoria(int codigo);
+        Task<ProdutoViewModel> ObterPorId(Guid id);
+        Task<IEnumerable<ProdutoViewModel>> ObterTodos();
+        Task<IEnumerable<CategoriaViewModel>> ObterCategorias();
+        Task AdicioanrProduto(ProdutoViewModel produtoViewModel);
+        Task AtualizarProduto(ProdutoViewModel produtoViewModel);
+        Task<ProdutoViewModel> DebitarEstoque(Guid guid, int quantidade);
+        Task<ProdutoViewModel> ReporEstoque(Guid, int quantidade);
+    }
+}
